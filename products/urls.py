2a68a99de8cdea633d098005d43e345993ea3685
muhammad usman <usmankhadim.uk.uk@gmail.com>
2026-06-22 22:39:27 +0500

    add stats api

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ProductListCreateView, ProductRetrieveUpdateDestroyView
+from .views import ProductListCreateView, ProductRetrieveUpdateDestroyView, StatsAPIView
 
 urlpatterns = [
     path('products/', ProductListCreateView.as_view(), name='product-list-create'),
     path('products/<int:pk>/', ProductRetrieveUpdateDestroyView.as_view(), name='product-detail'),
+    path('stats/', StatsAPIView.as_view(), name='stats'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,8 +1,11 @@
 from rest_framework import generics
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Product
 from .serializers import ProductSerializer
+from categories.models import Category
 
 
 class ProductListCreateView(generics.ListCreateAPIView):
@@ -17,3 +20,17 @@ class ProductListCreateView(generics.ListCreateAPIView):
 class ProductRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.select_related('category').all()
     serializer_class = ProductSerializer
+
+
+class StatsAPIView(APIView):
+    """
+    API endpoint to get statistics data including products count and categories count
+    """
+    def get(self, request):
+        products_count = Product.objects.count()
+        categories_count = Category.objects.count()
+        
+        return Response({
+            'products_count': products_count,
+            'categories_count': categories_count
+        })
