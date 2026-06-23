c609f1f70b0d140fa76e6816e3c6e7a7f0ef45a7
usman <muhammadusman67200@gmail.com>
2026-06-23 14:15:22 +0500

    remove banner app

--- banners/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Banner
-
-
-class BannerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Banner
-        fields = ['id', 'title', 'image', 'link', 'description', 'is_active', 'position', 'created_at', 'updated_at']
-        read_only_fields = ['created_at', 'updated_at']
--- banners/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import BannerListCreateView, BannerRetrieveUpdateDestroyView
-
-urlpatterns = [
-    path('banners/', BannerListCreateView.as_view(), name='banner-list-create'),
-    path('banners/<int:pk>/', BannerRetrieveUpdateDestroyView.as_view(), name='banner-detail'),
-]
